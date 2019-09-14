86a814672bfc0c65a3d62d4b381c6d0a4245ddca
Finn <finnterdal@gmail.com>
2019-09-14 11:44:48 -0500

    creates a code demo repo, but not if no name provided

--- create_code_demo.sh
+++ create_code_demo.sh
@@ -1,7 +1,14 @@
-function codesample {
+# Create a repo for a JS code demo
+function demo {
+  if [ $# -eq 0 ]
+    then
+      echo "No arguments supplied"
+      exit 1;
+  fi
   mkdir $1
   cd $1
   git init & npm init -y
   echo node_modules >> .gitignore
+  code .
 }
-alias codesample=codesample
+alias demo=demo
